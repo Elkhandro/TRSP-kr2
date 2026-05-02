28b7514ad22c7ac3902f1e3e0fc2b284605629b5
Elkhan Poladov <elkhanpoladov@gmail.com>
2026-05-02 14:59:45 +0300

    kr-2 v1

---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from routers import users, products, auth, headers
+
+app = FastAPI(title="TRSP-kr-2")
+
+# Подключаем модули
+app.include_router(users.router)
+app.include_router(products.router)
+app.include_router(auth.router)
+app.include_router(headers.router)
